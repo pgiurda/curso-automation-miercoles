703153e869b7efd7f71f301bf71d3a274206e28f
pgiurda <pgiurda@hotmail.com>
2025-10-20 00:25:28 -0300

    Se agrega practica integradora

---
+++ src/main/java/com/automatizacion/practicaClase3/Integrador.java
@@ -0,0 +1,25 @@
+package com.automatizacion.practicaClase3;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Integrador {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int numero;
+        List<Integer> listaNumeros = new ArrayList<>();
+        do {
+            System.out.println("Ingrese un numero");
+            numero = scanner.nextInt();
+            listaNumeros.add(numero);
+        } while(numero <= 500);
+        System.out.println("Datos ingresados: " + listaNumeros);
+        if (listaNumeros.size() < 10){
+            System.out.println("Se ingresaron pocos numeros");
+        } else {
+            System.out.println("Se excedio la cantidad de ingresos");
+        }
+    }
+}
